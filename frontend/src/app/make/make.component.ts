7024d63b07bb70e9c5fb05676b679ec1620ad085
Ivan Moslavac <ivan.moslavac@hotmail.com>
2018-08-28 12:22:04 +0200

    help n eeded

--- frontend/src/app/make/make.component.ts
+++ frontend/src/app/make/make.component.ts
@@ -10,34 +10,59 @@ export class MakeComponent implements OnInit {
 
   Sort = 'Name';
   Filter = '';
-  Page = '2';
-  pageSize = '3';
-  viewBy = 10;
+  Page = 1;
+  pageSize = 3;
+  pageStr = this.Page.toString();
+  pageSizeStr = this.pageSize.toString();
   totalItems;
-  Pages = this.totalItems / this.viewBy;
+  Pages = this.totalItems / this.pageSize;
   Ascending = 'true';
+  previousPage: any;
   makes: Make[];
 
   constructor(private carsService: CarsService) { }
 
   ngOnInit() {
-    this.carsService.getMakes(this.Sort, this.Filter, this.Page, this.pageSize, this.Ascending).subscribe(res => {
-      console.log(res);
-      this.makes = res;
-      this.totalItems = res.length;
-    });
-  }
-
-  setPage(pageNo) {
-    this.Page = pageNo;
-  }
-
-  setSize(size) {
-    this.pageSize = size;
+    this.getAll();
   }
 
   getMakes() {
-    this.carsService.getMakes(this.Sort, this.Filter, this.Page, this.pageSize, this.Ascending);
+    this.carsService.getMakes(this.Sort, this.Filter, this.pageStr, this.pageSizeStr, this.Ascending).subscribe(res => {
+      this.makes = res;
+      this.pageStr = this.Page.toString();
+      this.pageSizeStr = this.pageSize.toString();
+      this.Pages = this.totalItems / this.pageSize;
+      console.log(res);
+    });
+  }
+
+  getAll() {
+    this.carsService.getMakes(this.Sort, this.Filter, '1', '10000', this.Ascending).subscribe(res => {
+      this.totalItems = res.length;
+      this.Pages = this.totalItems / this.pageSize;
+      console.log(res);
+      this.getMakes();
+    });
+  }
+
+  pageChange(pageNo) {
+    this.Page = pageNo;
+    this.pageStr = this.Page.toString();
+    this.getMakes();
+    console.log(pageNo);
+  }
+
+  filter(search) {
+    console.log(search);
+    this.Filter = search;
+    this.getAll();
+  }
+
+  changeSize(size) {
+    console.log(size);
+    this.pageSize = size;
+    this.pageSizeStr = size.toString();
+    this.getAll();
   }
 
   deleteMake(id) {
